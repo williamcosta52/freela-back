9414ad70eff6aaecdef7ac0a3cc4af89f34e5a9a
william <wiliancosta51@hotmail.com>
2023-05-29 17:16:02 -0300

    cross origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(json());
 app.use(router);
 
